8c2abdc4443d1f362b62883a8ce2635a31ee95e9
Kseniia <balerinagizelle@mail.ru>
2021-11-22 00:15:50 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Введите N - количество строк и столбцов массива:   ");
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(n);
+
+            int[,] array = new int[n, m];
+           
+            
+            
+            Console.WriteLine("Вывод массива:");
+            for (int i = 0; i < n; i++)
+            {
+                for(int j=0;j<m; j++)
+
+                {
+
+                    array[i, j] = 1 - (i + j) % 2;
+                       
+                    Console.Write("{0,4}",array[i,j]);
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
